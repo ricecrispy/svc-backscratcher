ff60dae91bee3c23a19f750799fc560f6def86fc
Jeffrey Siu <jeffrey.siu@ihsmarkit.com>
2021-06-13 14:48:39 -0400

    feat: added health check controller

---
+++ src/svc-backscratcher/Controllers/HealthChecksController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace svc_backscratcher.Controllers
+{
+    [ApiController]
+    [Route("/health")]
+    public class HealthChecksController : ControllerBase
+    {
+        public HealthChecksController()
+        {
+
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult> GetHealthCheckResult()
+        {
+            return Ok();
+        }
+    }
+}
